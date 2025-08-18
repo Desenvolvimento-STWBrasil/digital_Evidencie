7b8a83b1780997b59e68e0be498d4e95388bc1c1
Desenvolvimento-STWBrasil <desenvolvimento@stwbrasil.com>
2025-08-18 11:13:15 -0300

    Ajuste layout

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -12,9 +12,9 @@ export default function Modal({ isOpen, onClose, title, children }: ModalProps) 
 
     return (
         // Fundo semi-transparente (backdrop)
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 m-4">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
             {/* Container do Modal */}
-            <div className="bg-white rounded-lg shadow-xl p-6 w-full max-w-md">
+            <div className="bg-white rounded-lg shadow-xl p-6 w-full max-w-md m-4">
                 <div className="flex justify-between items-center mb-4">
                     <h2 className="text-2xl font-bold text-titulo">{title}</h2>
                     <button onClick={onClose} className="text-gray-500 hover:text-gray-800">
